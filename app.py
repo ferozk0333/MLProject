f401a84c39db7f26063bb53bd7f38f27bb102633
ferozk0333 <ferozk0333@gmail.com>
2024-07-03 16:44:11 +0530

    Prediction Pipeline

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler   #to scale pickle file
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+
+application = Flask(__name__)       #entry point to application
+
+app = application
+
+# route for home page
+
+@app.route('/')
+def index():
+    print("Success1")
+    return render_template('index.html')
+
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():                                             #fetching data and returning output
+    print("Success12")
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:   #POST request
+        print("Success123")
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+
+        pred_df = data.get_data_as_data_frame()                         #function defined in predict_pipeline
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()                            #object of PredictPipeline class
+        results = predict_pipeline.predict(pred_df)                     #transfers flow to predict fn in predict_pipeline.py
+        return render_template('home.html', results = results[0])       #all the values are in list format, so results[0]
+    
+    #testing app
+    if __name__=="__main__":
+        app.run(host="0.0.0.0")  
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,59 @@
+import sys 
+import os 
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from src.utils import load_object  #to load pickle file
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+
+        try:
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'            #responsible for handling categorical features, feature scaling etc
+            model = load_object(file_path = model_path)                 #load pickle file, utils.py
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            pred = model.predict(data_scaled)
+            return pred
+        except Exception as e:
+            raise CustomException(e,sys)
+
+class CustomData:          #responsible for mapping html frontend inputs to the backend
+    def __init__(self,
+                 gender:str,
+                 race_ethnicity:str,   #not compulsory to provide datatype. Python is smart.
+                 parental_level_of_education,
+                 lunch: str,
+                 test_preparation_course: str,
+                 reading_score: int,
+                 writing_score: int):
+        
+
+            self.gender = gender
+            self.race_ethnicity = race_ethnicity
+            self.parental_level_of_education = parental_level_of_education
+            self.lunch = lunch
+            self.test_preparation_course = test_preparation_course
+            self.reading_score = reading_score
+            self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):           #returns input data as dataframe for backend processing
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
