f60b20695f248f1c93540d1353fbf9110402c6fe
ferozk0333 <ferozk0333@gmail.com>
2024-07-04 14:12:41 +0530

    requirements.txt updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements(file_path: str) -> List[str]:
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-
+    print(requirements)
     return requirements
 
 
